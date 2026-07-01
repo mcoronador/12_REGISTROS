911cc816d00a1bdbced1c6c88468f050e0138480
mcoronador <milagroscoronado16@gmail.com>
2026-07-01 09:40:09 -0500

    struct for agree.cpp

--- empledos.cpp
+++ empledos.cpp
@@ -10,5 +10,21 @@ float salario;
 };
 
 int main (){
-	empleados
+	empleados [100];
+	int n;
+	cin>>n;
+	
+	for (int i=0 ; i<n;i++)
+	{
+		cin>>empleados[i].numero;
+		cin>>empleados[i].nombre;
+		
+		for (int j=0; j<12; j++)
+		{
+		 cin>>empleado[i].ventas[j];
+		 
+		 }
+		}
+		
+	}
 }
