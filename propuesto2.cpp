abc4f18ec1a3d6db6b31b28274c3b77e20410c9a
mcoronador <milagroscoronado16@gmail.com>
2026-07-01 10:02:56 -0500

    declaracion de variables

---
+++ propuesto2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Persona
+{
+	string nombre;
+	string dni;
+	int edad;
+};
+
+int main(){
+	persona personas [100];
+	int n;
+	int mayores=0;
+	int suma=0;
+	float promedio;
+	 cin>>n;
+	 
+	for (int i=0;i<n;i++)
+	{
+		cin>>persona [i]
+	}
+}
