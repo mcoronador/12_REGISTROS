5668d06e83df5c835595bdce4e4c76bd19db1401
mcoronador <milagroscoronado16@gmail.com>
2026-07-01 09:29:46 -0500

    avance del archivo.cpp

---
+++ empledos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct empleado
+{ int numero ;
+string nombre;
+float ventas [12];
+float salario;
+};
+
+int main (){
+	empleados
+}
